9e2f946751ec31b3bed239047812ac3bb9c88ae0
Jason Barr <jason.barr@metronet.com>
2023-05-12 23:13:52 -0500

    InputStream class

---
+++ src/lexer/InputStream.js
@@ -0,0 +1,57 @@
+/**
+ * @class
+ * @desc Object to manage the state of the input to the program
+ * @property {string} input
+ * @property {number} pos
+ * @property {number} line
+ * @property {number} col
+ */
+export class InputStream {
+    /**
+     * Constructs the InputStream object
+     * @param {string} input
+     */
+    constructor(input) {
+        this.input = input;
+        this.pos = 0;
+        this.line = 1;
+        this.col = 1;
+    }
+
+    /**
+     * Static constructor
+     * @param {string} input
+     */
+    static new(input) {
+        return new InputStream(input);
+    }
+
+    get length() {
+        return this.input.length;
+    }
+
+    /**
+     * See what the character is n steps in front of the current position
+     * @param {number} n
+     * @returns {string}
+     */
+    lookahead(n = 1) {
+        return this.input[this.pos + n];
+    }
+
+    /**
+     * Get the character at the current position and advance the stream
+     * @returns {string}
+     */
+    next() {
+        return this.input[this.pos++];
+    }
+
+    /**
+     * Get the character at the current position in the input stream
+     * @returns {string}
+     */
+    peek() {
+        return this.input[this.pos];
+    }
+}
