61affcee3e7792e08d7788fe94f4543b6bf16e19
Jason Barr <jason.barr@metronet.com>
2023-05-12 23:52:26 -0500

    Add additional character matchers

--- src/lexer/utils.js
+++ src/lexer/utils.js
@@ -1,3 +1,6 @@
 export const isNumber = str => /^[+-]?\d+(\.\d+)?$/.test(str);
 export const isDot = ch => /\./.test(ch);
 export const isDigit = ch => /\d/.test(ch);
+export const isWhitespace = ch => /\s/.test(ch);
+export const isSemicolon = ch => /;/.test(ch);
+export const isNewline = ch => /\n/.test(ch);
