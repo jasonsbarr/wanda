f0c1701dbd97b506c7f614ae7c5ac0a277b24715
Jason Barr <jason.barr@metronet.com>
2023-05-15 13:39:46 -0500

    Add tests for reading numbers

---
+++ tests/reader/read.test.js
@@ -0,0 +1,27 @@
+import { Token } from "../../src/lexer/Token.js";
+import { tokenize } from "../../src/lexer/tokenize.js";
+import { read } from "../../src/reader/read.js";
+
+test("should read input as a parseTree list (array)", () => {
+  const input = "100";
+  const readResult = read(tokenize(input));
+
+  expect(Array.isArray(readResult)).toBe(true);
+});
+
+test("should read an integer from the token stream as a number token", () => {
+  const input = "23";
+  const readResult = read(tokenize(input))[0];
+
+  expect(readResult instanceof Token).toBe(true);
+  expect(readResult.type).toBe("Number");
+  expect(readResult.value).toBe("23");
+});
+
+test("should read a float from the token stream as a number token", () => {
+  const input = "6.28";
+  const readResult = read(tokenize(input))[0];
+
+  expect(readResult.type).toBe("Number");
+  expect(readResult.value).toBe("6.28");
+});
