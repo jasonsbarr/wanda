d31fb9339d0d14f15ee55f26d8d054814f34866a
Jason Barr <jason.barr@metronet.com>
2023-05-15 14:39:41 -0500

    Right now the desugarer just returns the AST

---
+++ src/desugarer/desugar.js
@@ -0,0 +1,8 @@
+import { AST } from "../parser/ast.js";
+
+/**
+ * Desugars the AST into an AST that contains only core forms
+ * @param {AST} ast
+ * @returns {AST}
+ */
+export const desugar = (ast) => ast;
