f24f88fb5fc511bcfb8f706e570119f31bb2fe1b
Jason Barr <jason.barr@metronet.com>
2023-05-15 16:05:45 -0500

    Only use parseExpr for now

--- src/parser/parse.js
+++ src/parser/parse.js
@@ -26,15 +26,6 @@ const parseExpr = (reader) => {
 };
 
 /**
- * Parses an expression from the readTree
- * @param {Reader} reader
- * @returns {AST}
- */
-const parseExpression = (reader) => {
-  return parseExpr(reader);
-};
-
-/**
  * Parses the reader-returned tree into a full AST
  * @param {import("../reader/read.js").ReadTree} readTree
  * @returns {AST}
@@ -45,7 +36,7 @@ export const parse = (readTree) => {
   const reader = Reader.new(readTree);
 
   while (!reader.eof()) {
-    body.push(parseExpression(reader));
+    body.push(parseExpr(reader));
   }
 
   return AST.Program(body);
