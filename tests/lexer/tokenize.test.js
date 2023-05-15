08beaea6536ab96559fdefd8fafe99e8b06c1b8e
Jason Barr <jason.barr@metronet.com>
2023-05-15 13:21:38 -0500

    Not using EOF tokens anymore, so don't need to account for it in test

--- tests/lexer/tokenize.test.js
+++ tests/lexer/tokenize.test.js
@@ -1,23 +1,23 @@
 import { tokenize } from "../../src/lexer/tokenize.js";
 
-test('should correctly tokenize an integer', () => {
+test("should correctly tokenize an integer", () => {
   const input = "15";
   const tokens = tokenize(input);
 
-  expect(tokens.reduce((output, tok) => tok.value !== "EOF" ? tok.type : output, "")).toEqual("Number");
-  expect(tokens.reduce((output, tok) => tok.value !== "EOF" ? tok.value : output, "")).toEqual("15");
+  expect(tokens.reduce((_, tok) => tok.type, "")).toEqual("Number");
+  expect(tokens.reduce((_, tok) => tok.value, "")).toEqual("15");
 });
 
 test("should correctly tokenize a decimal number", () => {
   const input = "3.14";
   const tokens = tokenize(input);
 
-  expect(tokens.reduce((output, tok) => tok.value !== "EOF" ? tok.type : output, "")).toEqual("Number");
-  expect(tokens.reduce((output, tok) => tok.value !== "EOF" ? tok.value : output, "")).toEqual("3.14");
+  expect(tokens.reduce((_, tok) => tok.type, "")).toEqual("Number");
+  expect(tokens.reduce((_, tok) => tok.value, "")).toEqual("3.14");
 });
 
 test("should throw a SyntaxException on invalid numeric input", () => {
   const input = "3.13.14";
 
   expect(() => tokenize(input)).toThrow();
-})
+});
