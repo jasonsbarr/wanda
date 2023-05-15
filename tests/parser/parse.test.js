953369ff5c87fca6bf41f6c584da4d5c5bbe36f4
Jason Barr <jason.barr@metronet.com>
2023-05-15 15:40:00 -0500

    Use expand function in test

--- tests/parser/parse.test.js
+++ tests/parser/parse.test.js
@@ -1,18 +1,19 @@
 import { tokenize } from "../../src/lexer/tokenize.js";
 import { read } from "../../src/reader/read.js";
+import { expand } from "../../src/expander/expand.js";
 import { parse } from "../../src/parser/parse.js";
 import { ASTTypes } from "../../src/parser/ast.js";
 
 test("should parse a Program node from the readTree", () => {
   const input = "16";
-  const ast = parse(read(tokenize(input)));
+  const ast = parse(expand(read(tokenize(input))));
 
   expect(ast.type).toEqual(ASTTypes.Program);
 });
 
 test("should parse a NumericLiteral node from a number token", () => {
   const input = "17.234";
-  const ast = parse(read(tokenize(input)));
+  const ast = parse(expand(read(tokenize(input))));
   const node = ast.body[0];
 
   expect(node.type).toEqual(ASTTypes.NumberLiteral);
