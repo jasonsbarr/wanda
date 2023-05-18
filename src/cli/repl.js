a8db12de8b533fd9ab410d9f33371acd4b956efc
Jason Barr <jason.barr@metronet.com>
2023-05-17 20:52:02 -0500

    Export repl and use try/catch

---
+++ src/cli/repl.js
@@ -0,0 +1,23 @@
+import readlineSync from "readline-sync";
+import { EVAL } from "./eval.js";
+import { print } from "../printer/print.js";
+
+const getInput = (prompt) => readlineSync.question(prompt);
+
+export const repl = () => {
+  let prompt = "wanda> ";
+
+  while (true) {
+    try {
+      let result = EVAL(getInput(prompt));
+
+      if (result === "") {
+        break;
+      }
+
+      console.log(print(result));
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
+};
