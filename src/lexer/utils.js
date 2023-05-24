bdbc4f834885824ade0886eceac7bc87c856c4aa
Jason Barr <jason.barr@metronet.com>
2023-05-23 20:53:08 -0500

    Add isPlus char matcher

--- src/lexer/utils.js
+++ src/lexer/utils.js
@@ -5,6 +5,7 @@ export const isWhitespace = (ch) => /\s/.test(ch);
 export const isSemicolon = (ch) => /;/.test(ch);
 export const isNewline = (ch) => /\n/.test(ch);
 export const isDash = (ch) => /\-/.test(ch);
+export const isPlus = (ch) => /\+/.test(ch);
 export const isDoubleQuote = (ch) => /"/.test(ch);
 export const isColon = (ch) => /:/.test(ch);
 export const isSymbolStart = (ch) => /[=<>%:|?\\/*\p{L}_$!+-]/u.test(ch);
