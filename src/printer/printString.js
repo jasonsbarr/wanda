94323d2ea1a67556e9ba1c5d91cd1a45892d431d
Jason Barr <jason.barr@metronet.com>
2023-05-24 15:54:49 -0500

    Print additional primitive types

--- src/printer/printString.js
+++ src/printer/printString.js
@@ -4,6 +4,18 @@ export const printString = (value) => {
   switch (typeof value) {
     case "number":
       return String(value);
+    case "string":
+      return `"` + value + `"`;
+    case "symbol":
+      return value.description;
+    case "boolean":
+      return String(value);
+    case "undefined":
+      return "nil";
+    case "object":
+      if (value === null) {
+        return "nil";
+      }
     default:
       throw new Exception(`Invalid value ${value}`);
   }
