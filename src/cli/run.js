b8e34102413399271465174e1ada03badac6e11d
Jason Barr <jason@jasonsbarr.com>
2023-05-27 17:53:26 -0500

    Add bin file

--- src/cli/run.js
+++ src/cli/run.js
@@ -1,5 +1,6 @@
 import { repl } from "./repl.js";
 
-const mode = process.argv.length >= 3 ? process.argv[2] : "repl";
-
-repl(mode);
+export const run = () => {
+  const mode = process.argv.length >= 3 ? process.argv[2] : "repl";
+  repl(mode);
+}
