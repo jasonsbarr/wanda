d3fdad1e60c7eda0a806185b34c2bb4130905d9c
Jason Barr <jason@jasonsbarr.com>
2023-05-27 20:15:45 -0500

    Use switch statement to parse CLI

--- src/cli/run.js
+++ src/cli/run.js
@@ -1,6 +1,18 @@
 import { repl } from "./repl.js";
 
 export const run = () => {
-  const mode = process.argv.length >= 3 ? process.argv[2] : "repl";
+  let mode = ""
+  switch (process.argv[2]) {
+    case "print":
+      if (process.argv[3] === "-d") {
+        mode = "printDesugared"
+        break;
+      } else if (process.argv[3] === "-a") {
+        mode = "printAST"
+        break;
+      }
+    default:
+      mode = "repl"
+  }
   repl(mode);
 }
