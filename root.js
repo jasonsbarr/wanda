b323d0d649074c0bfd4c6d6da0ea7fcdbd5cf76f
Jason Barr <jason.barr@metronet.com>
2023-06-05 10:22:39 -0500

    Use path.dirname to make sure joining root path works correctly

--- root.js
+++ root.js
@@ -1,4 +1,5 @@
+import path from "path";
 import { fileURLToPath } from "url";
 
 export const ROOT_URL = import.meta.url;
-export const ROOT_PATH = fileURLToPath(ROOT_URL);
+export const ROOT_PATH = fileURLToPath(path.dirname(ROOT_URL));
