a286f1fde975146f511d357ead69469cf61770a6
Jason Barr <jason.barr@metronet.com>
2023-06-05 20:42:52 -0500

    Docblock

--- src/cli/compileAndBuild.js
+++ src/cli/compileAndBuild.js
@@ -3,6 +3,20 @@ import { emitGlobalEnv } from "../emitter/emitGlobalEnv.js";
 import { build } from "./build.js";
 import { compile } from "./compile.js";
 
+/**
+ * @typedef Options
+ * @prop {string} fileName the file being compiled
+ * @prop {Function} contextCreator the function to create the surrounding environment
+ * @prop {Function} nsCreator the function to create the namespace for compilation
+ * @prop {string} outName the out name of the temporary build file
+ * @prop {string} moduleName the variable name for the compiled module output
+ */
+/**
+ * Compiles a module of Wanda code and builds it with its surrounding environment
+ * @param {string} wandaCode
+ * @param {Options} opts
+ * @returns {string}
+ */
 export const compileAndBuild = (
   wandaCode,
   {
