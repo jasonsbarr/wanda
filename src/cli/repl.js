5e989a33d565a09897f844de85d7e014f9c2388e
Jason Barr <jason.barr@metronet.com>
2023-06-06 11:19:38 -0500

    Rewrite REPL to work with global module

--- src/cli/repl.js
+++ src/cli/repl.js
@@ -1,33 +1,53 @@
+import vm from "vm";
 import readlineSync from "readline-sync";
-import { EVAL } from "./eval.js";
 import { pprintAST, pprintDesugaredAST } from "./pprint.js";
 import { print } from "../printer/print.js";
+import { makeGlobalNameMap } from "../runtime/makeGlobals.js";
+import { emitGlobalEnv } from "../emitter/emitGlobalEnv.js";
 import { fail } from "../shared/fail.js";
+import { build } from "./build.js";
+import { compile } from "./compile.js";
 
 const read = (prompt) => readlineSync.question(prompt);
 
-export const repl = (mode) => {
-  const proc =
-    mode === "repl"
-      ? EVAL
-      : mode === "printDesugared"
-      ? pprintDesugaredAST
-      : mode === "printAST"
-      ? pprintAST
-      : fail("Invalid REPL mode specified");
+export const repl = (mode = "repl") => {
+  // Create global compile environment
+  const compileEnv = makeGlobalNameMap();
+
+  // Build global module and instantiate in REPL context
+  // This should make all compiled global symbols available
+  const globalCode = build(emitGlobalEnv());
+  vm.runInThisContext(globalCode);
+
   let prompt = "wanda> ";
 
   while (true) {
     try {
       const input = read(prompt);
 
-      if (input === ":quit") {
-        process.exit(0);
+      switch (input) {
+        // If it's a command, execute it
+        case ":quit":
+          process.exit(0);
+        case ":print-ast":
+          mode = "printAST";
+          break;
+        case ":print-d":
+          mode = "printDesugared";
+          break;
+        // If it's code, compile and run it
+        default:
+          let compiled = compile(input, "stdin", compileEnv);
+          let result = vm.runInThisContext(compiled);
+
+          if (mode === "printAST") {
+            pprintAST(input);
+          } else if (mode === "printDesugared") {
+            pprintDesugaredAST(input);
+          }
+
+          print(result);
       }
-
-      let result = proc(input);
-
-      print(result, mode === "repl");
     } catch (e) {
       console.error(e.message);
     }
