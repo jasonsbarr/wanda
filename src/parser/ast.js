e036326cdfdfbf45510eea1692abc2bb037b1dcd
Jason Barr <jason.barr@metronet.com>
2023-06-10 15:35:22 -0500

    Add DoExpression to AST type union

--- src/parser/ast.js
+++ src/parser/ast.js
@@ -61,7 +61,7 @@ export const ASTTypes = {
  * @typedef {NumberLiteral|StringLiteral|BooleanLiteral|KeywordLiteral|NilLiteral} Primitive
  */
 /**
- * @typedef {Program|NumberLiteral|StringLiteral|BooleanLiteral|KeywordLiteral|NilLiteral|Symbol|CallExpression|VariableDeclaration|SetExpression} AST
+ * @typedef {Program|NumberLiteral|StringLiteral|BooleanLiteral|KeywordLiteral|NilLiteral|Symbol|CallExpression|VariableDeclaration|SetExpression|DoExpression} AST
  */
 export const AST = {
   /**
