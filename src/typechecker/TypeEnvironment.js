ab227bc28c0f58e28740fc501b3d42da5bb9d7a7
Jason Barr <jason.barr@metronet.com>
2023-06-10 16:25:10 -0500

    Define types for types

--- src/typechecker/TypeEnvironment.js
+++ src/typechecker/TypeEnvironment.js
@@ -17,11 +17,72 @@ export class TypeEnvironment extends Namespace {
   }
 
   /**
+   * Checks if type name exists anywhere in the environment chain
+   * @param {string} name
+   * @returns {boolean}
+   */
+  existsType(name) {
+    return this.lookupType(name) !== null;
+  }
+
+  /**
    * Constructs a child out of the current type environment
    * @param {string} name
    * @returns {TypeEnvironment}
    */
   extend(name) {
     return new TypeEnvironment(this, { name });
+  }
+
+  /**
+   * Returns a type from the TypeEnvironment
+   * @param {string} name
+   * @returns {import("./types.js").Type}
+   */
+  getType(name) {
+    const scope = this.lookupType(name);
+
+    if (scope) {
+      return scope.types.get(name);
+    }
+
+    return null;
+  }
+
+  /**
+   * Checks if a type name exists in the current environment
+   * @param {string} name
+   * @returns {boolean}
+   */
+  hasType(name) {
+    return this.types.has(name);
+  }
+
+  /**
+   * Looks up if a type exists in the environment chain
+   * @param {string} name
+   * @returns {TypeEnvironment|null}
+   */
+  lookupType(name) {
+    let scope = this;
+
+    while (scope) {
+      if (scope.types.has(name)) {
+        return scope;
+      }
+
+      scope = scope.parent;
+    }
+
+    return null;
+  }
+
+  /**
+   * Sets a type in the current TypeEnvironment
+   * @param {string} name
+   * @param {import("./types.js").Type} type
+   */
+  setType(name, type) {
+    this.types.set(name, type);
   }
 }
---
+++ src/typechecker/types.js
@@ -0,0 +1,46 @@
+/**
+ * @enum {string}
+ */
+export const TypeTypes = {
+  Number: "Number",
+  String: "String",
+  Boolean: "Boolean",
+  Keyword: "Keyword",
+  Nil: "Nil",
+  TypeAlias: "TypeAlias",
+  List: "List",
+};
+/**
+ * @typedef Number
+ * @prop {TypeTypes.Number} kind
+ */
+/**
+ * @typedef String
+ * @prop {TypeTypes.String} kind
+ */
+/**
+ * @typedef Boolean
+ * @prop {TypeTypes.Boolean} kind
+ */
+/**
+ * @typedef Keyword
+ * @prop {TypeTypes.Keyword} kind
+ */
+/**
+ * @typedef Nil
+ * @prop {TypeTypes.Nil} kind
+ */
+/**
+ * @typedef TypeAlias
+ * @prop {TypeTypes.TypeAlias} kind
+ * @prop {string} name
+ * @prop {Type} base
+ */
+/**
+ * @typedef List
+ * @prop {TypeTypes.List} kind
+ * @prop {Type} listType
+ */
+/**
+ * @typedef {Number|String|Boolean|Keyword|Nil|TypeAlias|List} Type
+ */
