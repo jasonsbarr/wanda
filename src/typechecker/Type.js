96b77f27e88d17d8e5060f638b9ad323cc3df661
Jason Barr <jason.barr@metronet.com>
2023-06-10 17:12:33 -0500

    Add fromTypeAnnotation to Type module

--- src/typechecker/Type.js
+++ src/typechecker/Type.js
@@ -1,9 +1,11 @@
 import { TypeTypes } from "./types.js";
 import * as Validators from "./validators.js";
 import * as Constructors from "./constructors.js";
+import { fromTypeAnnotation } from "./fromTypeAnnotation.js";
 
 export const Type = {
   Type: TypeTypes,
   ...Constructors,
   ...Validators,
+  fromTypeAnnotation,
 };
