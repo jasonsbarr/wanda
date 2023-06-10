b736d48543361caaa6b0e2038d27fee5a6b60ce2
Jason Barr <jason.barr@metronet.com>
2023-06-10 18:58:41 -0500

    Don't need other properties on function object

--- src/runtime/makeFunction.js
+++ src/runtime/makeFunction.js
@@ -1,15 +1,7 @@
 import { Type } from "../typechecker/Type.js";
 
 export const makeFunction = (func) => {
-  func.variadic = true;
   func.contract = Type.functionType([Type.any], Type.any, true);
-
-  Object.defineProperty(func, "length", {
-    enumerable: false,
-    writable: false,
-    configurable: false,
-    value: 1,
-  });
 
   return func;
 };
