7065783e5070a159d7bc13bd7813fac97cae01cb
Jason Barr <jason.barr@metronet.com>
2023-06-10 16:39:36 -0500

    Create consolidated type module

---
+++ src/typechecker/Type.js
@@ -0,0 +1,11 @@
+import { TypeTypes } from "./types.js";
+import * as Validators from "./validators.js";
+import * as Constructors from "./constructors.js";
+
+export const Type = {
+  Type: TypeTypes,
+  ...Constructors,
+  ...Validators,
+};
+
+console.log(Type);
