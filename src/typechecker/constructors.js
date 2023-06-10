65596ae0c0ebe7ff0c4a09377b17108e43c44bf1
Jason Barr <jason.barr@metronet.com>
2023-06-10 16:35:28 -0500

    Create type constructors

---
+++ src/typechecker/constructors.js
@@ -0,0 +1,45 @@
+import { TypeTypes } from "./types.js";
+
+/**
+ * @type {import("./types").Number}
+ */
+export const number = { kind: TypeTypes.Number };
+
+/**
+ * @type {import("./types").String}
+ */
+export const string = { kind: TypeTypes.String };
+
+/**
+ * @type {import("./types").Boolean}
+ */
+export const boolean = { kind: TypeTypes.Boolean };
+
+/**
+ * @type {import("./types").Keyword}
+ */
+export const keyword = { kind: TypeTypes.Keyword };
+
+/**
+ * @type {import("./types").Nil}
+ */
+export const nil = { kind: TypeTypes.Nil };
+
+/**
+ * Type Alias constructor
+ * @param {string} name
+ * @param {import("./types").Type} base
+ * @returns {import("./types").TypeAlias}
+ */
+export const typeAlias = (name, base) => ({
+  kind: TypeTypes.TypeAlias,
+  name,
+  base,
+});
+
+/**
+ * List type constructor
+ * @param {import("./types").Type} listType
+ * @returns {import("./types").List}
+ */
+export const listType = (listType) => ({ kind: TypeTypes.List, listType });
