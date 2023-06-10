662d28e22d3e865b5b179190fc00ef058cc5d75c
Jason Barr <jason.barr@metronet.com>
2023-06-10 18:52:44 -0500

    Set dummy info on functions so library functions don't break type checker

--- src/runtime/makeFunction.js
+++ src/runtime/makeFunction.js
@@ -1,1 +1,12 @@
-export const makeFunction = (func) => func;
+export const makeFunction = (func) => {
+  func.variadic = true;
+
+  Object.defineProperty(func, "length", {
+    enumerable: false,
+    writable: false,
+    configurable: false,
+    value: 1,
+  });
+
+  return func;
+};
