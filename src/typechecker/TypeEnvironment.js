33a0573beba74372bc90fd3073d9aeafe82e0e26
Jason Barr <jason.barr@metronet.com>
2023-06-10 16:08:54 -0500

    New and extend methods for Type Environment

--- src/typechecker/TypeEnvironment.js
+++ src/typechecker/TypeEnvironment.js
@@ -1,1 +1,27 @@
 import { Namespace } from "../shared/Namespace.js";
+
+/**
+ * @class TypeEnvironment
+ * @extends Namespace
+ * @desc Environment for type checking
+ * @prop {Map<string, Type>} types
+ */
+export class TypeEnvironment extends Namespace {
+  constructor(parent = null, { name = "global" } = {}) {
+    super(parent, { name });
+    this.types = new Map();
+  }
+
+  static new(parent = null, { name = "global" } = {}) {
+    return new TypeEnvironment(parent, { name });
+  }
+
+  /**
+   * Constructs a child out of the current type environment
+   * @param {string} name
+   * @returns {TypeEnvironment}
+   */
+  extend(name) {
+    return new TypeEnvironment(this, { name });
+  }
+}
