95f4dd792b0eea3e431f6f590fb5c9c84ad86be2
Jason Barr <jason.barr@metronet.com>
2023-06-10 19:52:29 -0500

    Make global type env for CLI

---
+++ src/typechecker/makeGlobalTypeEnv.js
@@ -0,0 +1,12 @@
+import { makeGlobal } from "../runtime/makeGlobals.js";
+import { Type } from "./Type.js";
+import { TypeEnvironment } from "./TypeEnvironment.js";
+
+export const makeGlobalTypeEnv = () => {
+  const globals = makeGlobal();
+  const typeEnv = TypeEnvironment.new(null, { name: "global" });
+
+  for (let [k, v] of globals) {
+    typeEnv.set(k, v.contract ?? Type.any);
+  }
+};
