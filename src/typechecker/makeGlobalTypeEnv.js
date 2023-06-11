584397b9e2d9cb1d0a8341661d3d04171c978ce3
Jason Barr <jason.barr@metronet.com>
2023-06-10 20:36:37 -0500

    Use Corelib to construct TypeEnv

--- src/typechecker/makeGlobalTypeEnv.js
+++ src/typechecker/makeGlobalTypeEnv.js
@@ -1,12 +1,13 @@
-import { makeGlobal } from "../runtime/makeGlobals.js";
+import { theModule as Core } from "../../lib/js/core.js";
+import { makeRuntime } from "../runtime/makeRuntime.js";
 import { Type } from "./Type.js";
 import { TypeEnvironment } from "./TypeEnvironment.js";
 
 export const makeGlobalTypeEnv = () => {
-  const globals = makeGlobal();
+  const mod = Core.module(makeRuntime());
   const typeEnv = TypeEnvironment.new(null, { name: "global" });
 
-  for (let [k, v] of globals) {
+  for (let [k, v] of Object.entries(mod)) {
     typeEnv.set(k, v.contract ?? Type.any);
   }
 
