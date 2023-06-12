c4314dd97d2f032da0ba9e9651154093be4fecdb
Jason Barr <jason.barr@metronet.com>
2023-06-12 18:36:00 -0500

    Subtype checking for vector and object types

---
+++ src/typechecker/propType.js
@@ -0,0 +1,7 @@
+/**
+ * Gets the type of an object property if it exists
+ * @param {import("./types").Object} type
+ * @param {string} name
+ * @returns {import("./types").Type|null}
+ */
+export const propType = (type, name) => {};
