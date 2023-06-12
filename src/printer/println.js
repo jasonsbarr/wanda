9915a3f68902d56b792888bbb1b381a6326be5b0
Jason Barr <jason.barr@metronet.com>
2023-06-12 15:43:41 -0500

    Use process.stdout.write for println as well

--- src/printer/println.js
+++ src/printer/println.js
@@ -1,4 +1,4 @@
 import { printString } from "./printString.js";
 
 export const println = (input, withQuotes = true) =>
-  console.log(printString(input, withQuotes));
+  process.stdout.write(printString(input, withQuotes)) + "\n";
