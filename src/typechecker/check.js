02248e429ec19f450062ec77ac3ac5f221e43c3c
Jason Barr <jason.barr@metronet.com>
2023-06-17 18:07:20 -0500

    Check function types

--- src/typechecker/check.js
+++ src/typechecker/check.js
@@ -17,6 +17,14 @@ export const check = (ast, type, env) => {
     return checkObject(ast, type, env);
   }
 
+  if (
+    (ast.kind === ASTTypes.FunctionDeclaration ||
+      ast.kind === ASTTypes.LambdaExpression) &&
+    Type.isFunctionType(type)
+  ) {
+    return checkFunction(ast, type, env);
+  }
+
   const inferredType = infer(ast, env);
 
   if (!isSubtype(inferredType, type)) {
@@ -30,7 +38,7 @@ export const check = (ast, type, env) => {
 };
 
 /**
- *
+ * Checks an object type
  * @param {import("../parser/ast.js").RecordLiteral} ast
  * @param {import("./types").Object} type
  * @param {TypeEnvironment} env
@@ -64,4 +72,40 @@ const checkObject = (ast, type, env) => {
 
     check(expr, pType, env);
   });
+};
+
+/**
+ * Checks a function type
+ * @param {import("../parser/ast.js").FunctionDeclaration|import("../parser/ast.js").LambdaExpression} ast
+ * @param {import("./types").FunctionType} type
+ * @param {TypeEnvironment} env
+ */
+const checkFunction = (ast, type, env) => {
+  if (type.params.length !== ast.params.length) {
+    throw new TypeException(
+      `Expected ${type.params.length} args; got ${ast.params.length}`,
+      ast.srcloc
+    );
+  }
+
+  const funcType = infer(ast, env);
+
+  type.params.forEach((p, i) => {
+    const pType = funcType.params[i];
+    if (!isSubtype(pType, p)) {
+      throw new TypeException(
+        `${Type.toString(pType)} is not a valid subtype of ${Type.toString(p)}`,
+        ast.srcloc
+      );
+    }
+  });
+
+  if (!isSubtype(funcType.ret, type.ret)) {
+    throw new TypeException(
+      `${Type.toString(funcType.ret)} is not a valid subtype of ${Type.toString(
+        type.ret
+      )}`,
+      ast.srcloc
+    );
+  }
 };
