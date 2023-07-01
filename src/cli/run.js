fe91506d7739ce78e1e9f8ab1eb1efeda701b8a7
Jason Barr <jason.barr@metronet.com>
2023-06-30 21:36:04 -0500

    Add ability to run file from command line

--- src/cli/run.js
+++ src/cli/run.js
@@ -1,6 +1,13 @@
+import vm from "vm";
+import fs from "fs";
 import { join } from "path";
 import { Exception } from "../shared/exceptions.js";
 import { repl } from "./repl.js";
+import { makeGlobalNameMap } from "../runtime/makeGlobals.js";
+import { emitGlobalEnv } from "../emitter/emitGlobalEnv.js";
+import { build } from "./build.js";
+import { compile } from "./compile.js";
+import { makeGlobalTypeEnv } from "../typechecker/makeGlobalTypeEnv.js";
 
 export const run = () => {
   switch (process.argv[2]) {
@@ -15,7 +22,23 @@ export const run = () => {
     case "repl":
       repl();
       break;
+    case "run":
+      if (!process.argv[3]) {
+        throw new Exception(`run command requires file path as argument`);
+      }
+      return runFile(join(process.cwd(), process.argv[3]));
     default:
       throw new Exception("Invalid command specified");
   }
+};
+
+const runFile = (path) => {
+  const fileContents = fs.readFileSync(path, { encoding: "utf-8" });
+  const globalNs = makeGlobalNameMap();
+  const typeEnv = makeGlobalTypeEnv();
+  const globalCode = build(emitGlobalEnv());
+  const compiledCode = compile(fileContents, path, globalNs, typeEnv);
+
+  vm.runInThisContext(globalCode);
+  return vm.runInThisContext(compiledCode);
 };
