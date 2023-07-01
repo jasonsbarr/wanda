691a6a675d52e8f249e94fb819415e9f5898a64a
Jason Barr <jason.barr@metronet.com>
2023-06-30 21:10:14 -0500

    Remove print command

--- src/cli/run.js
+++ src/cli/run.js
@@ -4,21 +4,13 @@ import { repl } from "./repl.js";
 export const run = () => {
   let mode = "";
   switch (process.argv[2]) {
-    case "print":
-      if (process.argv[3] === "-d") {
-        mode = "printDesugared";
-        break;
-      } else if (process.argv[3] === "-a") {
-        mode = "printAST";
-        break;
-      }
     case undefined:
     case "-i":
     case "repl":
       mode = "repl";
+      repl(mode);
       break;
     default:
       throw new Exception("Invalid command specified");
   }
-  repl(mode);
 };
