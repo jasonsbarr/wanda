89f8ef3c0180fed3dd74401455cb9acabf24824f
Jason Barr <jason.barr@metronet.com>
2023-06-30 22:28:50 -0500

    Add wandac utility for compilation

---
+++ bin/wandac.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import os from "os";
+import fs from "fs";
+import { join, basename } from "path";
+import { compile } from "../src/cli/compile.js";
+import { build } from "../src/cli/build.js";
+import { makeGlobalNameMap } from "../src/runtime/makeGlobals.js";
+import { makeGlobalTypeEnv } from "../src/typechecker/makeGlobalTypeEnv.js";
+import { emitGlobalEnv } from "../src/emitter/emitGlobalEnv.js";
+import { Exception } from "../src/shared/exceptions.js";
+
+if (!process.argv[2]) {
+  throw new Exception(`wandac requires either a file path or command argument`);
+}
+
+switch (process.argv[2]) {
+  case "build": {
+    const pathname = join(process.cwd(), process.argv[3]);
+    const compiledFile = compileFile(pathname);
+    const globals = emitGlobalEnv();
+    const code = globals + os.EOL + os.EOL + compiledFile;
+    const outfile = basename(pathname) + ".js";
+    const built = build(code, outfile, basename(pathname));
+
+    fs.writeFileSync(outfile, built, { encoding: "utf-8" });
+    break;
+  }
+  default: {
+    // should be a file path
+    const pathname = join(process.cwd(), process.argv[2]);
+    const compiledFile = compileFile(pathname);
+    const globals = emitGlobalEnv();
+    const code = globals + os.EOL + os.EOL + compiledFile;
+    const outfile = basename(pathname) + ".js";
+
+    fs.writeFileSync(outfile, code, { encoding: "utf-8" });
+    break;
+  }
+}
+
+const compileFile = (path) => {
+  const contents = fs.readFileSync(path, { encoding: "utf-8" });
+  return compile(contents, path, makeGlobalNameMap(), makeGlobalTypeEnv());
+};
