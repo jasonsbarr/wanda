69e5fcf6100dbd2c8080ebb7ab9b9b62df046c76
Jason Barr <jason.barr@metronet.com>
2023-07-10 11:37:46 -0500

    Add __module__ type in parseTypeObject function since every module will have it

--- src/runtime/parseTypesObject.js
+++ src/runtime/parseTypesObject.js
@@ -1,6 +1,8 @@
 import { parseContract } from "./parseContract.js";
 
 export const parseTypesObject = (types) => {
+  types.__module__ = "string";
+
   const parsedTypes = Object.fromEntries(
     Object.entries(types).map(([k, v]) => [k, parseContract(v)])
   );
