695f10e6f2068730e9f5dfdb11690d822e0ceeeb
Jason Barr <jason.barr@metronet.com>
2023-07-10 10:19:34 -0500

    Change conflicting variable name

--- src/runtime/parseTypesObject.js
+++ src/runtime/parseTypesObject.js
@@ -1,10 +1,10 @@
 import { parseContract } from "./parseContract.js";
 
 export const parseTypesObject = (types) => {
-  const types = Object.fromEntries(
+  const parsedTypes = Object.fromEntries(
     Object.entries(types).map(([k, v]) => [k, parseContract(v)])
   );
 
   types.kind = "Module";
-  return types;
+  return parsedTypes;
 };
