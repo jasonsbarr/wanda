b9dc77b02506502d45a0763f3a8b06dacfde15ef
Jason Barr <jason.barr@metronet.com>
2023-07-09 20:34:04 -0500

    Add values and types properties to Module object

--- src/runtime/Module.js
+++ src/runtime/Module.js
@@ -5,6 +5,8 @@
  * @prop {Function} module the module constructor
  * @prop {string[]} requires in-lang required modules
  * @prop {string[]} nativeRequires native JS required modules
+ * @prop {Object} values values provided by a module and their types
+ * @prop {Object} types types provided by a module
  */
 class Module {
   /**
@@ -13,12 +15,16 @@ class Module {
    * @param {Function} module the module constructor
    * @param {string[]} requires in-lang required modules
    * @param {string[]} nativeRequires native JS required modules
+   * @param {Object} values
+   * @param {Object} types
    */
-  constructor(name, module, requires, nativeRequires) {
+  constructor(name, module, requires, nativeRequires, values, types) {
     this.name = name;
     this.module = module;
     this.requires = requires;
     this.nativeRequires = nativeRequires;
+    this.values = values;
+    this.types = types;
   }
 
   toString() {
@@ -32,7 +38,15 @@ class Module {
  * @param {Function} module the module constructor
  * @param {string[]} requires in-lang required modules
  * @param {string[]} nativeRequires native JS required modules
+ * @param {Object} values
+ * @param {Object} types
  * @returns {Module}
  */
-export const makeModule = (name, module, requires = [], nativeRequires = []) =>
-  new Module(name, module, requires, nativeRequires);
+export const makeModule = (
+  name,
+  module,
+  requires = [],
+  nativeRequires = [],
+  values = {},
+  types = {}
+) => new Module(name, module, requires, nativeRequires, values, types);
