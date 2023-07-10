8830c0756c67bdc6d01aa6f9fbd958adb1f836b2
Jason Barr <jason.barr@metronet.com>
2023-07-09 22:08:57 -0500

    Add kind of Module to parsed type object

--- src/runtime/parseTypesObject.js
+++ src/runtime/parseTypesObject.js
@@ -1,6 +1,10 @@
 import { parseContract } from "./parseContract.js";
 
-export const parseTypesObject = (types) =>
-  Object.fromEntries(
+export const parseTypesObject = (types) => {
+  const types = Object.fromEntries(
     Object.entries(types).map(([k, v]) => [k, parseContract(v)])
   );
+
+  types.kind = "Module";
+  return types;
+};
