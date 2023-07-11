ef14348bfb6655c99b7c3ae663a8b32524feaf96
Jason Barr <jason.barr@metronet.com>
2023-07-11 14:52:57 -0500

    Handle case where types object already contains types

--- src/runtime/parseTypesObject.js
+++ src/runtime/parseTypesObject.js
@@ -4,7 +4,11 @@ export const parseTypesObject = (types) => {
   types.__module__ = "string";
 
   const parsedTypes = Object.fromEntries(
-    Object.entries(types).map(([k, v]) => [k, parseContract(v)])
+    Object.entries(types).map(([k, v]) => [
+      k,
+      // if it's a string, it's a string type annotation - otherwise it's already a Type
+      typeof v === "string" ? parseContract(v) : v,
+    ])
   );
 
   types.kind = "Module";
