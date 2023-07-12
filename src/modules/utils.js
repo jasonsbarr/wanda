cdb1c1c927d5ad0d9fc9a2edb32ba8ac2fe6f1df
Jason Barr <jason.barr@metronet.com>
2023-07-12 14:13:48 -0500

    Dependency name can be MemberExpression node

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -3,7 +3,7 @@ import { resolve } from "./resolve.js";
 /**
  * Resolves arrays of required module import specifiers/specifier strings to file locations
  * @param {string[]} requires
- * @returns {{name: string; source: string}[]}
+ * @returns {{name: string|import("../parser/ast.js").MemberExpression; source: string}[]}
  */
 export const getModulePaths = (requires) =>
   requires.map((req) => ({ name: req, source: resolve(req) }));
