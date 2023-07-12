d4a7008db4ff9fc1c222e38bba3730a095d7ea05
Jason Barr <jason.barr@metronet.com>
2023-07-12 14:19:22 -0500

    Add provides property to ModuleCompilation object

--- src/modules/ModuleCompilation.js
+++ src/modules/ModuleCompilation.js
@@ -9,15 +9,17 @@ export class ModuleCompilation {
    * @param {string} [opts.outPath=""]
    * @param {boolean} [opts.global=false]
    * @param {{name: string|import("../parser/ast.js").MemberExpression; source: string}[]} [dependencies=[]]
+   * @param {import("../parser/ast.js").Symbol[]} opts.provides
    */
   constructor(
     code,
     sourcePath,
-    { outPath = "", global = false, dependencies = [] } = {}
+    { outPath = "", global = false, dependencies = [], provides = [] } = {}
   ) {
     this.code = code;
     this.sourcePath = sourcePath;
     this.dependencies = dependencies;
+    this.provides = provides;
 
     if (!this.sourcePath.endsWith(".wanda")) {
       this.outPath = this.sourcePath;
@@ -36,17 +38,19 @@ export class ModuleCompilation {
    * @param {string} [opts.outPath=""]
    * @param {boolean} [opts.global=false]
    * @param {{name: string|import("../parser/ast.js").MemberExpression; source: string}[]} [opts.dependencies=[]]
+   * @param {import("../parser/ast.js").Symbol[]} opts.provides
    * @returns {ModuleCompilation}
    */
   static new(
     code,
     sourcePath,
-    { outPath = "", global = false, dependencies = [] } = {}
+    { outPath = "", global = false, dependencies = [], provides = [] } = {}
   ) {
     return new ModuleCompilation(code, sourcePath, {
       outPath,
       global,
       dependencies,
+      provides,
     });
   }
 }
