4781623155c397778f26755a02d731bade40ff7f
Jason Barr <jason@jasonsbarr.com>
2024-05-31 13:37:36 -0500

    That's not going to work

--- src/js/module/loader.js
+++ src/js/module/loader.js
@@ -11,29 +11,6 @@ let moduleTable = {};
 let nameMap = {};
 let modules = {};
 
-const fetchModuleDependencies = (module) => {
-  let requires = [];
-
-  if (module instanceof NativeModule) {
-    const { requires: dependencies } = module;
-    requires = dependencies.map((dep) => {
-      const node = convertNativeRequireToNode(dep);
-      return Require.new(
-        node,
-        module.name,
-        resolveModuleImport(node),
-        module.name
-      );
-    });
-  } else if (module instanceof ModuleCompilation) {
-    requires = module.requires;
-  } else {
-    throw new Exception(`Unknown module ${module}`);
-  }
-
-  return requires;
-};
-
 const getLoadOrder = (deps) => {
   let sorted = [];
   let toVisit = {};
