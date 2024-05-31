57e8b910accdc8b3e1d9e15d11628847792e4fea
Jason Barr <jason@jasonsbarr.com>
2024-05-31 13:50:24 -0500

    Add output location to ModuleCompilation object

--- src/js/module/ModuleCompilation.js
+++ src/js/module/ModuleCompilation.js
@@ -5,12 +5,14 @@ export class ModuleCompilation {
    * @param {string} location
    * @param {import("../parser/ast.js").Program} ast
    * @param {import("./Require.js").Require[]} requires
+   * @param {string} outputLocation
    */
-  constructor(name, location, ast, requires) {
+  constructor(name, location, ast, requires, outputLocation) {
     this.name = name;
     this.location = location;
     this.ast = ast;
     this.requires = requires;
+    this.outputLocation = outputLocation;
   }
 
   /**
@@ -19,9 +21,10 @@ export class ModuleCompilation {
    * @param {string} location
    * @param {import("../parser/ast.js").Program} ast
    * @param {import("./Requires.js").Requires} requires
+   * @param {string} outputLocation
    * @returns {ModuleCompilation}
    */
-  static new(name, location, ast, requires) {
-    return new ModuleCompilation(name, location, ast, requires);
+  static new(name, location, ast, requires, outputLocation) {
+    return new ModuleCompilation(name, location, ast, requires, outputLocation);
   }
 }
